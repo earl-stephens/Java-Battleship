12c9802e38c528b6b61b33fb4d42119fa6bb5c2e
earl-stephens <stephensearl@hotmail.com>
2023-01-30 16:53:00 -0700

    Test and code for Cell object

---
+++ src/application/Cell.java
@@ -0,0 +1,9 @@
+package application;
+
+public class Cell {
+	public String coordinate;
+	
+	public Cell(String coordinate) {
+		this.coordinate = coordinate;
+	}
+}
---
+++ src/test/CellTest.java
@@ -0,0 +1,17 @@
+package test;
+import application.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+class CellTest {
+	Cell cell = new Cell("B4");
+	
+	@Test
+	void testForCellObject() {
+		Assert.assertNotNull(cell);
+	}
+
+}
