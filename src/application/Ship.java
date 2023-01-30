32a1702abd71f50b5e864a3cc20263364de1cd4d
earl-stephens <stephensearl@hotmail.com>
2023-01-30 16:30:36 -0700

    Test and code for Ship object

---
+++ src/application/Ship.java
@@ -0,0 +1,11 @@
+package application;
+
+public class Ship {
+	public String name;
+	public int length;
+	
+	public Ship(String name, int length) {
+		this.name = name;
+		this.length = length;
+	}
+}
---
+++ src/test/ShipTest.java
@@ -0,0 +1,17 @@
+package test;
+import application.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+class ShipTest {
+	Ship ship = new Ship("Cruiser", 3);
+	
+	@Test
+	void testForShipObject() {
+		Assert.assertNotNull(ship);
+	}
+
+}
