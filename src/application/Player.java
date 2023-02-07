f02d503cae3f2bc5cf4ca35b787d389c34e2fac3
earl-stephens <stephensearl@hotmail.com>
2023-02-06 22:58:04 -0700

    Add showPlacementMessages and create ship objects in constructor

--- src/application/Player.java
+++ src/application/Player.java
@@ -3,10 +3,14 @@ package application;
 public class Player {
 	String name;
 	public Board board;
+	Ship cruiser;
+	Ship submarine;
 	
 	public Player(String name) {
 		this.name = name;
 		board = new Board();
+		cruiser = new Ship("Cruiser", 3);
+		submarine = new Ship("Submarine", 2);
 	}
 	
 	public boolean placeShips() {
@@ -22,5 +26,17 @@ public class Player {
 		}
 	}
 	
+	public void showPlacementMessages() {
+		System.out.println("I have laid out my ships on the grid.");
+		System.out.println("You now need to lay out your two ships.");
+		System.out.println("The cruiser is 3 spaces long and the submarine is two spaces long.");
+		System.out.println("Lay out the ships horizontally (from left to right) or");
+		System.out.println("Vertically (from to top to bottom).");
+		
+		placeCruiser(cruiser);
+	}
 	
+	public void placeCruiser(Ship ship) {
+		
+	}
 }
